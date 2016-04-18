5346a158d29b275d62f0c790b75f00f743487a6b
wattanar <iamwattanas@gmail.com>
2016-04-19 01:48:18 +0700

    exit when matched

--- src/Router.php
+++ src/Router.php
@@ -10,17 +10,13 @@
 			$currentUri = self::getUri();
 			
 			if ($currentUri === $mapUri) {
-
 				if (is_callable(array($fn[0], $fn[1]))) {
-
 					call_user_func(array($fn[0], $fn[1]));
+					exit;
 				} else {
-
 					exit('<pre>404 not found.</pre>');
 				}
-				
 			} else {
-
 				exit('<pre>404 not found.</pre>');
 			}
 		}
@@ -28,23 +24,18 @@
 		public function isTrustMethod($method)
 		{
 			if ($method !== $_SERVER['REQUEST_METHOD']) {
-
 				exit('<pre>404 not found.</pre>');
 			}
-
 			return true;
 		}
 
 		public function getUri($subDir = null)
 		{
 			if ($subDir === null) {
-
 				$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 			} else {
-
 				$uri = str_replace($subDir, "", parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 			}
-
 			return $uri;
 		}
 	}
