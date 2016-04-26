0e1e2d246d33cfba63a4b02a02145452e8338838
wattanar <iamwattanas@gmail.com>
2016-04-27 00:26:12 +0700

    add uri matching

--- src/Router.php
+++ src/Router.php
@@ -17,19 +17,33 @@
 		{
 			$url = explode("?", str_replace($url, "", $_SERVER["REQUEST_URI"]))[0];
 			$url = str_replace("//", "/", $url);
-
-			if (!array_key_exists($url, self::$routes)) {
+			// route exists
+			if (array_key_exists($url, self::$routes)) {
+				// match route method
+				if (self::$methods[$url] !== $_SERVER["REQUEST_METHOD"]) {
+					exit("<pre>Method not allow.</pre>");
+				}
+				// call function
+				call_user_func(self::$routes[$url]);
+				exit;
+			} else {
+				// Loop routes
+				foreach (self::$routes as $route => $callback) {
+					// Check params
+					if (preg_match('#^' . $route . '$#', $url, $matched)) {
+						// match route method
+						if (self::$methods[$route] === $_SERVER["REQUEST_METHOD"]) {
+							// shift out $matched[0]
+							array_shift($matched);
+							call_user_func_array($callback, $matched);
+							exit;
+						}
+						// Method not match
+						exit("<pre>Method not allow.</pre>");
+					}
+				}
+				// route not found
 				exit("<pre>404 not found.</pre>");
 			}
-
-			if (!is_callable(self::$routes[$url])) {
-				exit("<pre>404 not found.</pre>");
-			}
-
-			if (self::$methods[$url] !== $_SERVER["REQUEST_METHOD"]) {
-				exit("<pre>Method not allow.</pre>");
-			}
-
-			call_user_func(self::$routes[$url]);
 		}
 	}
