73a7b24d799540230eaf5fcf84ac78a8b3ddc910
Wattana Ruengmucha <wattanar@users.noreply.github.com>
2016-04-26 15:27:53 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,9 @@
 <?php
-	
-	require './vendor/autoload.php';
-	require './bootstrap.php';
+
+	require './src/Router.php';
 
 	use Gnix\Core\Router as R;
-	
-	R::addRoute('GET', '/', ['my_class', 'my_function']);
-	
-	R::notFound(function() {
-		echo "404 not found.";
-	});
+
+	R::addRoute("GET", "/", array("my_class", "my_function"));
+
+	R::start("/if_your_app_in_sub_folder");
