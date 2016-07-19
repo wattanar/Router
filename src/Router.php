2c04a6d827fcef84525c979c484cd3c2f6933f7e
Wattana Ruengmucha <wattanar@users.noreply.github.com>
2016-07-19 14:01:24 +0700

    Update Router.php

--- src/Router.php
+++ src/Router.php
@@ -10,27 +10,27 @@ class Router
 	public static function get($pattern, $callback) 
 	{
 		self::$routes[$pattern] = $callback;
-		self::$methods[$pattern] = 'GET';
+		self::$methods[$pattern] = "GET";
 	}
 
 	public static function post($pattern, $callback) 
 	{
 		self::$routes[$pattern] = $callback;
-		self::$methods[$pattern] = 'POST';
+		self::$methods[$pattern] = "POST";
 	}
 
 	public static function run($url = null)
 	{
-		$url = explode('?', str_replace($url, '', $_SERVER['REQUEST_URI']))[0];
+		$url = explode("?", str_replace($url, "", $_SERVER["REQUEST_URI"]))[0];
 		$url = str_replace("//", "/", $url);
 		// route exists
 		if (array_key_exists($url, self::$routes)) {
 			// match route method
 			if (self::$methods[$url] !== $_SERVER["REQUEST_METHOD"]) {
-				exit('<pre>Method not allow.</pre>');
+				exit("<pre>Method not allow.</pre>");
 			}
 			// Instanitate controller
-			$obj = explode('::', self::$routes[$url]);
+			$obj = explode("::", self::$routes[$url]);
 			// call function
 			$controller = new $obj[0]();
 			$controller->$obj[1]();
@@ -39,22 +39,22 @@ class Router
 			// Loop routes
 			foreach (self::$routes as $route => $callback) {
 				// Check params
-				if (preg_match('#^' . $route . '$#', $url, $matched)) {
+				if (preg_match("#^" . $route . "$#", $url, $matched)) {
 					// match route method
-					if (self::$methods[$route] !== $_SERVER['REQUEST_METHOD']) {
+					if (self::$methods[$route] !== $_SERVER["REQUEST_METHOD"]) {
 						// Method not match
-						exit('<pre>Method not allow.</pre>');
+						exit("<pre>Method not allow.</pre>");
 					}
 					// shift out $matched[0]
 					array_shift($matched);
 					// Instanitate controller
-					$obj = explode('::', $callback);
+					$obj = explode("::", $callback);
 					call_user_func_array([new $obj[0](), $obj[1]], $matched);
 					exit;
 				}
 			}
 			// route not found
-			exit('<pre>404 not found.</pre>');
+			exit("<pre>404 not found.</pre>");
 		}
 	}
 }
