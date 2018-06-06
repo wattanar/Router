a9de39fa15b5e0016ebcbd46f14b2fba6ce7d557
Wattana Ruengmucha <iamwattanas@gmail.com>
2018-06-06 16:55:38 +0700

    add custom separate string

--- src/Router.php
+++ src/Router.php
@@ -6,6 +6,7 @@ class Router
 {
 	private static $routes = [];
 	private static $methods = [];
+	private static $separate = "::";
 
 	public static function get($pattern, $callback) 
 	{
@@ -31,40 +32,47 @@ class Router
 		self::$methods[$pattern] = "DELETE";
 	}
 
+	public static function setSeparate($separateString) {
+		self::$separate = $separateString;
+	}
+
 	public static function run($url = null)
 	{
 		$url = explode("?", str_replace($url, "", $_SERVER["REQUEST_URI"]))[0];
 		$url = str_replace("//", "/", $url);
-		// route exists
+
 		if (array_key_exists($url, self::$routes)) {
-			// match route method
+
 			if (self::$methods[$url] !== $_SERVER["REQUEST_METHOD"]) {
 				exit("<pre>Method not allow.</pre>");
 			}
-			// Instanitate controller
-			$obj = explode("::", self::$routes[$url]);
-			// call function
+
+			$obj = explode(self::$separate, self::$routes[$url]);
+			
 			$controller = new $obj[0];
+			
 			$func = $obj[1];
+
 			exit($controller->$func());
+
 		} else {
-			// Loop routes
+
 			foreach (self::$routes as $route => $callback) {
-				// Check params
+
 				if (preg_match("#^" . $route . "$#", $url, $matched)) {
-					// match route method
+
 					if (self::$methods[$route] !== $_SERVER["REQUEST_METHOD"]) {
-						// Method not match
 						exit("<pre>Method not allow.</pre>");
 					}
-					// shift out $matched[0]
+
 					array_shift($matched);
-					// Instanitate controller
-					$obj = explode("::", $callback);
+					
+					$obj = explode(self::$separate, $callback);
+
 					exit(call_user_func_array([new $obj[0](), $obj[1]], $matched));
 				}
 			}
-			// route not found
+			
 			exit("<pre>404 not found.</pre>");
 		}
 	}
